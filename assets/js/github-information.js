0887cb29a9048e97582a49400cf0a1068ab23807
andreidrobota <drobotaandrei@gmail.com>
2024-01-11 14:39:52 +0000

    Fix minor bugs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -36,6 +36,9 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation (event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+    
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub Username!</h2>`);
@@ -69,3 +72,5 @@ function fetchGitHubInformation (event) {
         }
     )
 }
+
+$(document).ready(fetchGitHubInformation)
